6dd3e9abd01f3837408b550b5f4a5b0186919870
Hiago Alecsander <hiagosantana01@hotmail.com>
2025-02-15 21:00:05 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function gerarCurriculo() {
+    document.getElementById("cvNome").textContent = document.getElementById("nome").value;
+    document.getElementById("cvEmail").textContent = document.getElementById("email").value;
+    document.getElementById("cvTelefone").textContent = document.getElementById("telefone").value;
+    document.getElementById("cvExperiencia").textContent = document.getElementById("experiencia").value;
+}
